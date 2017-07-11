7691712a3b7b9c282fc9686d9ad814133a3cb90a
Vinnie Magro <vmagro@fb.com>
2017-07-10 21:22:10 -0700

    Move config to lib

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 const port = app.get('port') || 3000;
 
-const render = require('./render').default;
+const render = require('./lib/render').default;
 
 app.use(morgan('dev'));
 app.use('/static', express.static('../client/dist'));
