f5bb6fe0f5a0face8a409ad81adecfeaa0df7f65
Vinnie Magro <vmagro@fb.com>
2017-07-12 20:21:39 -0700

    Use cluster

--- server.js
+++ server.js
@@ -5,9 +5,18 @@ require('babel-polyfill');
 const winston = require('winston');
 
 const app = require('./lib/app').default;
-
 const port = app.get('port') || 3000;
 
-app.listen(port, () => {
-  winston.info('Listening on port ' + port);
-});
+const cluster = require('cluster');
+const cpus = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  winston.info('[master] Spawning ' + cpus + ' workers');
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
+} else {
+  app.listen(port, () => {
+    winston.info('[worker] Listening on port ' + port);
+  });
+}
