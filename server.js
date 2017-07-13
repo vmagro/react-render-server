21fe6ba93f0300d66bfce8cf461148f6ecff8d5a
Vinnie Magro <vmagro@fb.com>
2017-07-12 19:22:16 -0700

    Unit proving functionality

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const morgan = require('morgan');
-
 // this is probably really bad for production, look into later
 require('babel-register')({presets: ['env', 'react']});
 require('babel-polyfill');
+
+const winston = require('winston');
 
 const app = require('./lib/app').default;
 
 const port = app.get('port') || 3000;
 
 app.listen(port, () => {
-  console.log('Listening on port ' + port);
+  winston.info('Listening on port ' + port);
 });
